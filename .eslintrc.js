84fbc5fc505be74c17d0e1bc83fad9ed3317fc4b
Samuel Spek <samuel.spek@gmail.com>
2019-04-23 14:25:30 +0200

    Adjusted typescript eslint rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,8 +13,11 @@ module.exports = {
         '@typescript-eslint',
     ],
     'rules': {
-        "interface-name": false,
-        "no-unused-expression": [true, "allow-fast-null-checks"],
-        "object-literal-sort-keys": [true, "shorthand-first"]
+        "@typescript-eslint/explicit-member-accessibility": 0,
+        "@typescript-eslint/no-explicit-any": 0,
+        "@typescript-eslint/explicit-function-return-type": 0,
+        "@typescript-eslint/no-non-null-assertion": 0,
+        "@typescript-eslint/array-type": 0,
+        "@typescript-eslint/camelcase": 0,
     }
 };
