bc702bfe250cf942a4555edf8dbf2a3a812e497a
Samuel Spek <samuel.spek@gmail.com>
2019-06-04 12:22:55 +0200

    Changed printWidth to the default value.

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@ module.exports =  {
     semi:  false,
     trailingComma:  "none",
     singleQuote:  true,
-    printWidth:  120,
+    printWidth:  80,
     tabWidth:  2,
     arrowParens: "avoid",
 };
